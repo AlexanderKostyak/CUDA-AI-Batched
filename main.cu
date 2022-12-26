26a7469653ca2e5ebd3299c00b6499ddf7f29275
Alexander Kostyak <118746865+AlexanderKostyak@users.noreply.github.com>
2022-12-26 10:17:13 -0500

    Add files via upload

--- main.cu
+++ main.cu
@@ -1,6 +1,6 @@
 
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
 
 #include <vector>
 #include <iostream>
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
 
 	const unsigned int inputlayersize = 10;
 	const unsigned int outputlayersize = 12;
-	const unsigned int batchsizearg = 1;
+	const unsigned int batchsizearg = 10;
 	const float learningrate = 0.1;
 
 
@@ -77,15 +77,14 @@ int main(int argc, char** argv)
 
 
 
-	std::vector<float> single_input { 0.15, 0.45, .78, 0.04, 0.45, 0.73, 1, 0.11, 0.01, 0.11 };
-	std::vector<float> single_input_2 {0.38, 0.92, 0.16, 0.63, 0.82, 0.11, .2, 0.73, 0.25, 0.68};
+	std::vector<float> single_input { -0.15, 0.45, .78, 0.04, -0.45, -0.73, 1, -0.11, 0.01, 0.11 };
+	std::vector<float> single_input_2 {0.38, -0.92, 0.16, -0.63, 0.82, 0.11, .2, 0.73, -0.25, 0.68};
 	
-	std::vector<float> input_values{ 0.15, 0.45, .78, 0.04, 0.45, 0.73, 1, 0.11, 0.01, 0.11 };
-	std::vector<float> input_values_2{ 0.38, 0.92, 0.16, 0.63, 0.82, 0.11,.2, 0.73, 0.25, 0.68 };
+	std::vector<float> input_values{ -0.15, 0.45, .78, 0.04, -0.45, -0.73, 1, -0.11, 0.01, 0.11 };
+	std::vector<float> input_values_2{ 0.38, -0.92, 0.16, -0.63, 0.82, 0.11,.2, 0.73, -0.25, 0.68 };
 
 	std::vector<float> output_values { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, .9, 1.0, 0.9, 1.0 };            
 	std::vector<float> output_values_2 { 0.3, 0.9, 0.5, 0.0, 0.7, 0.4, 0.4, 0.1, 0.2, 0.3, 0.8, 0.3 };           
-
 
 	std::vector<float> output_valuest{ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8,.9, 1.0 , 0.9, 1.0 };            
 	std::vector<float> output_values_2t{ 0.3, 0.9, 0.5, 0.0, 0.7, 0.4, 0.4, 0.1,0.2, 0.3, 0.8, 0.3 };           
@@ -112,7 +111,7 @@ int main(int argc, char** argv)
 	long int before = GetTickCount();
 
 
-	std::cout << std::endl << std::endl << "batch size:   " << network.getBatchSize() << std::endl << "input layer size:   " << "should be 10" << std::endl << std::endl;
+	std::cout << std::endl << std::endl << "batch size:   " << network.getBatchSize() << std::endl << std::endl;
 	for (int i = 0; i < 500; i++)
 	{
 		network.train(input_values, output_values);
